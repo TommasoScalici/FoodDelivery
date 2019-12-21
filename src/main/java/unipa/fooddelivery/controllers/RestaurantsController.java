0317ed9e7f65c965013e630740cb6940fd4b080a
Salvatore Apicella <salvatore.apicella@outlook.com>
2019-12-21 15:12:21 +0100

    Salvo non funziona

--- src/main/java/unipa/fooddelivery/controllers/RestaurantsController.java
+++ src/main/java/unipa/fooddelivery/controllers/RestaurantsController.java
@@ -1,39 +1,33 @@
 package unipa.fooddelivery.controllers;
 
 import java.util.ArrayList;
+import java.util.Locale.Category;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.*;
 
-import unipa.fooddelivery.DataBase;
-import unipa.fooddelivery.models.*;
+import unipa.fooddelivery.*;
+import unipa.fooddelivery.models.Restaurant;
+import unipa.fooddelivery.models.RestaurantCategory;
 
 @Controller
 public class RestaurantsController {
 
-    @GetMapping(value = "/restaurants")
-    public ModelAndView getRestaurantsView() 
-    {
+    @GetMapping(value = "/restaurants/{category}")
+    public ModelAndView getRestaurantsView(@PathVariable RestaurantCategory category) 
+    {   
+        
         var mav = new ModelAndView("index");
         mav.addObject("path", "restaurants");
 
-        var restaurants = new ArrayList<Restaurant>();
-        restaurants.add(new Restaurant("La Dolce Vita", RestaurantCategory.ITALIAN, 10));
-        restaurants.add(new Restaurant("L'Arte Bianca", RestaurantCategory.PIZZA, 0));
-        restaurants.add(new Restaurant("Chan Sushi Bar", RestaurantCategory.SUSHI, 18));
-
-        var dishes = new ArrayList<Dish>();
-        dishes.add(new Dish("Lasagne", restaurants.get(0), DishCategory.FIRST, 10));
-        dishes.add(new Dish("Margherita", restaurants.get(1), DishCategory.PIZZA, 5));
-        dishes.add(new Dish("Sashimi", restaurants.get(2), DishCategory.SECOND, 14));
-
-        DataBase.getInstance().getDishes().addAll(dishes);
-        DataBase.getInstance().saveChangesForEntity(Dish.class);
-
-        DataBase.getInstance().getRestaurants().addAll(restaurants);
-        DataBase.getInstance().saveChangesForEntity(Restaurant.class);
-
+        var dishes = DataBase.getInstance().getDishes();
+        var dishe = dishes.stream()
+                        .filter(x -> x.getRestaurant().equals(category))
+                        .findFirst();
+              
         return mav;
     }
 }
