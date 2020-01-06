ef6d873c476bd79e17f58998bbe142360991388b
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2020-01-06 14:36:20 +0100

    Final Development Commit

--- src/main/java/unipa/fooddelivery/controllers/OrderController.java
+++ src/main/java/unipa/fooddelivery/controllers/OrderController.java
@@ -1,6 +1,9 @@
 package unipa.fooddelivery.controllers;
 
 import unipa.fooddelivery.*;
+import unipa.fooddelivery.models.*;
+
+import javax.servlet.http.*;
 
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +14,19 @@ import org.springframework.web.servlet.*;
 public class OrderController {
 
     @GetMapping()
-    public ModelAndView getOrdersView() {
-
-        var orders = DataBase.getInstance().getOrders();
+    public ModelAndView getOrdersView(HttpSession session) {
 
         var mav = new ModelAndView("index");
+        var user = session.getAttribute("user");
+
+        if(!(user instanceof Customer) || !(user instanceof DeliveryMan))
+        {
+            // Qui in teoria dovrebbe dare errore, ma come?
+        }
+            
+        var orders = DataBase.getInstance().getOrders();
+
+        
         mav.addObject("path", "orders");
         mav.addObject("orders", orders);
         return mav;
